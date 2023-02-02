c9b8534f2b6ad013b500cea9922be127b7edba53
sshevya <37146723+sshevya@users.noreply.github.com>
2023-02-02 13:53:11 +0530

    backtrack | imp

--- permutation.cpp
+++ permutation.cpp
@@ -1,3 +1,39 @@
+/****** Backtrack ******/
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& ans, vector<int>& currans,vector<int>& nums, int i){
+        if(i==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        if(i>=nums.size()){
+            return;
+        }
+        for(int j = i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            backtrack(ans,currans,nums,i+1);
+            swap(nums[i],nums[j]);
+        
+        }
+
+
+    }
+    
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> ans;
+    vector<int> currans;
+    backtrack(ans,currans,nums,0);
+    return ans;    
+    }
+};
+
+
+
+
+
+/*using function*/
+
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
