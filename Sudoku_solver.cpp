37da5343c8887cd9d3d225400ed27d37d6cec9f5
sshevya <37146723+sshevya@users.noreply.github.com>
2023-07-09 11:22:48 +0530

    Update Sudoku_solver.cpp

--- Sudoku_solver.cpp
+++ Sudoku_solver.cpp
@@ -1,3 +1,94 @@
+/********* First solution *********/
+/*****Store empty places initially instead of running loop every time*******/
+class Solution {
+public:
+    bool isValidRow(int row, int col, vector<vector<char>>& board){
+
+        for(int i=0;i<9;i++){
+            if(i==col){
+                continue;
+            }
+            if(board[row][i]==board[row][col]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isValidCol(int row, int col, vector<vector<char>>& board){
+        
+        for(int i=0;i<9;i++){
+            if(i==row){
+                continue;
+            }
+            if(board[i][col]==board[row][col]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isValidGrid(int row, int col, vector<vector<char>>& board){
+
+        int startRow = row - row%3;
+        int startCol = col - col%3;
+
+        for(int i = startRow; i<startRow+3;i++){
+            for(int j = startCol; j<startCol+3;j++){
+                if(i==row && j==col){
+                continue;
+                }
+                if(board[i][j]==board[row][col]){
+                return false;
+            }
+            }
+
+        }
+        return true;
+
+    }
+
+
+    bool backtrack(vector<vector<char>>& board){
+
+        int row = -1;
+        int col = -1;
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j]=='.'){
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        if(row == -1 && col == -1){
+            return true;
+        }
+
+        for(char i='1'; i<='9'; i++){
+            board[row][col] = i;
+            if(isValidRow(row,col,board) && isValidCol(row,col,board) && isValidGrid(row,col,board)){
+                if(backtrack(board)){
+                    return true;
+                }
+            }
+            board[row][col] = '.';
+
+        }
+        return false;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        backtrack(board);
+    }
+};
+
+
+
+/********** Second Solution**********/
 class Solution {
 public:
     
