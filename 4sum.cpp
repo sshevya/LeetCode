7e6ec6c2c6816200acda6b407d652b15c939bc05
sshevya <37146723+sshevya@users.noreply.github.com>
2023-12-08 14:34:34 +0530

    Array | Imp

--- 4sum.cpp
+++ 4sum.cpp
@@ -1,3 +1,58 @@
+/****** first sol ************/
+/******static cast into long long ******/
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        unordered_map<int,vector<pair<int,int>>> m;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int p = j+1;
+                int q = n-1;
+                while(p<q){
+                    long long sum = static_cast<long long>(nums[i])+nums[j]+nums[p]+nums[q];
+                    if(target==sum){
+                        if(i!=p && i!=q && j!=p && j!=q){
+                            vector<int> v;
+
+                            v.push_back(nums[i]);
+                            v.push_back(nums[j]);
+                            v.push_back(nums[p]);
+                            v.push_back(nums[q]);
+                            sort(v.begin(), v.end());
+                            if(find(ans.begin(),ans.end(),v)==ans.end()){
+                                    ans.push_back(v);
+                            }
+                            p++;
+                            q--;
+                        }
+                        
+                    }else if(target>sum){
+                        p++;
+                    }else{
+                        q--;
+                    }
+                }
+                
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+/****** Second sol, this sol gives TLE *******/
 class Solution {
 public:
    
