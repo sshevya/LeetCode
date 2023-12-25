02ba35b217933572b5830b9be9d249da34834db8
sshevya <37146723+sshevya@users.noreply.github.com>
2023-12-25 12:15:32 +0530

    DP | good

--- WordBreak.cpp
+++ WordBreak.cpp
@@ -29,3 +29,39 @@ public:
         return dp[n];
     }
 };
+
+
+/******** solution 2***********/
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<string, int> m;
+        int n= wordDict.size();
+        int l = s.length();
+
+        for(int i=0;i<n;i++){
+            m[wordDict[i]]=1;
+        }
+
+        vector<vector<int>> dp(l+1, vector<int>(l+1,0));
+        
+        for(int i=0;i<=l;i++){
+            //j shouldn't be till l because it represents length
+            for(int j=0;j<=l-i;j++){
+                string subs=s.substr(i,j);
+                if(m.find(subs)!=m.end()){
+                    dp[i][i+j]=1;
+                    if(dp[0][i]){
+                        dp[0][i+j] = 1;
+                    }
+                    
+                }
+            }
+        }
+    
+        
+        return dp[0][l];
+        
+    }
+};
