ac764da79d82e44f4c2e0c9e63596ac759d9ea4a
sshevya <37146723+sshevya@users.noreply.github.com>
2024-01-19 12:59:34 +0530

    Tree | BFS

---
+++ BinaryTreeZigZagLevelOrderTraversal.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        list<pair<TreeNode*, int>> q;
+        int level = 0;
+        q.push_back(make_pair(root, level));
+        int prev_level = -1;
+        vector<int> v;
+        TreeNode* curr = root;
+        bool flag = true;
+        while(!q.empty() && curr!=NULL){
+
+            curr = q.front().first;
+            level = q.front().second;
+            if(prev_level!=-1 && prev_level<level){
+                
+                ans.push_back(v);
+                v.clear();
+                flag = !flag;
+            }
+            if(flag){
+                v.push_back(curr->val);
+            }else{
+                v.insert(v.begin(), curr->val);
+            }
+            
+            cout<<curr->val<<" ";
+            q.pop_front();
+
+            if(curr->left != NULL){
+                q.push_back(make_pair(curr->left, level+1));
+            }
+            
+            if(curr->right!= NULL){
+                q.push_back(make_pair(curr->right, level+1));
+            }
+            prev_level = level;
+        }
+        
+        if(!v.empty()){
+            ans.push_back(v);
+        }
+
+        return ans;
+    }
+};
