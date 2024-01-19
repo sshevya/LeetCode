f6028688c0df60d314b6b8ff6fbb1b30bbccc61f
sshevya <37146723+sshevya@users.noreply.github.com>
2024-01-19 12:16:47 +0530

    Tree | Morris Traversal | O(1) Inorder | Threaded Tree

--- RecoverBST.cpp
+++ RecoverBST.cpp
@@ -1,5 +1,89 @@
 
-/******* Morris Traversal for O(1) spoace **********/
+/******* Morris Traversal for O(1) space **********/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode* val1 = NULL ;
+        TreeNode* val2 = NULL;
+        TreeNode* prev = NULL;
+        TreeNode* predecessor = NULL;
+
+        stack<TreeNode*> s;
+       // vector<int> v;
+        while(root!=NULL){
+            if(root->left == NULL){
+                /** compare element **/
+                if(val1 == NULL && (prev == NULL || prev->val>=root->val)){
+                    val1 = prev;
+                }
+
+                if(val1 != NULL && (prev == NULL || prev->val>=root->val)){
+                    val2 = root;
+                }
+
+                prev = root;
+                root = root->right;
+            }else{
+                predecessor = root->left;
+
+                /* First find inorder predecessor, rightmost child of left subtree*/
+
+                while(predecessor->right!=NULL && predecessor->right!=root){
+                    predecessor = predecessor->right;
+                }
+
+                /* If not threaded, thread by making right child of this rightmost child as curr 
+                *  go to left subtree of current
+                *   else revert to original
+                **/
+
+                if(predecessor->right==NULL){
+                    predecessor->right = root;
+                    root = root->left;
+                }else{
+                    /* restore modified tree to original tree */
+                    predecessor->right = NULL;
+                    /**compare element */
+                    if(val1 == NULL && (prev == NULL || prev->val>=root->val)){
+                        val1 = prev;
+                    }
+
+                    if(val1 != NULL && (prev == NULL || prev->val >= root->val)){
+                        val2 = root;
+                    }
+                    prev = root;
+                    root = root->right;
+                }
+            }
+        }
+
+
+        cout<<val1->val<<" "<<val2->val<<" ";
+        int temp = val1->val;
+        val1->val = val2->val;
+        val2->val = temp;
+        
+        
+    }
+};
+
+
+
+
+/***** Iterative Inorder O(n) space ***********/
 class Solution {
 public:
     void recoverTree(TreeNode* root) {
