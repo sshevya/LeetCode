e9bc39456e7c21a725d758c97be71caf99fdb546
sshevya <37146723+sshevya@users.noreply.github.com>
2024-01-21 11:21:14 +0530

    Tree | Recursion

--- FlattenBinaryTree.cpp
+++ FlattenBinaryTree.cpp
@@ -1,3 +1,62 @@
+/**** less complicated ******/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* rec(TreeNode* root){
+        
+        TreeNode* curr = root;
+        
+        if(root==NULL){
+            return NULL;
+        }
+
+        TreeNode* right = rec(root->right);
+
+        TreeNode* left = rec(root->left);
+
+        root->right = left;
+        root->left = NULL;
+        
+        while(root->right!=NULL){
+            root=root->right;
+        }
+        
+        root->right = right;
+
+
+
+        return curr;
+    }
+
+
+    void flatten(TreeNode* root) {
+
+        root = rec(root);
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+/********* old sol ******/
 class Solution {
 public:
     
