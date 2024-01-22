888ec7058f88b8fe04233daad9f68f3d4143ed9a
sshevya <37146723+sshevya@users.noreply.github.com>
2024-01-22 10:14:42 +0530

    Tree | LCA | IMP

---
+++ LowestCommonAncestorOfBinarySearchTree.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    bool findPath(TreeNode* root, vector<TreeNode*> &path, TreeNode* node){
+        if(root==NULL){
+            return false;
+        }
+        cout<<root->val<<" ";
+        path.push_back(root);
+
+        if(root->val == node->val){
+            return true;
+        }
+
+
+        if((root->left && findPath(root->left, path, node)) || (root->right && findPath(root->right, path, node))){
+            return true;
+        }
+
+        path.pop_back();
+        return false;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        vector<TreeNode*> path1;
+        vector<TreeNode*> path2;
+
+        
+        findPath(root, path1, p);
+        
+        findPath(root, path2, q);
+        
+        int ind = 0;
+        int i = 0;
+        cout<<"here "<<path1.size()<<" "<<path2.size();
+        for(i=0;i<path1.size() && i<path2.size();i++){
+            //cout<<path1[i]->val<<" ";
+            if(path1[i]->val!=path2[i]->val){
+                return path1[i-1];
+            }
+        }
+
+        if(i<path1.size()){
+            return path1[i-1];
+        }
+
+        if(i<path2.size()){
+            return path2[i-1];
+        }
+        return NULL;
+    }
+};
