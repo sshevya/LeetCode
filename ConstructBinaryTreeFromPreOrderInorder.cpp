7c342b2e422608ce60e882d767b5006728ff3182
sshevya <37146723+sshevya@users.noreply.github.com>
2024-01-23 10:28:36 +0530

    Tree | Recursion | Pass by reference

--- ConstructBinaryTreeFromPreOrderInorder.cpp
+++ ConstructBinaryTreeFromPreOrderInorder.cpp
@@ -1,3 +1,59 @@
+/********** no need to find index of root pass by reference ******/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findIndex(vector<int> &inorder, int root){
+        int n = inorder.size();
+
+        for(int i=0;i<n;i++){
+            if(root == inorder[i]){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    TreeNode* rec(vector<int>& preorder, vector<int> &inorder, int &i, int l, int r){
+        
+        if(l>r || i>=preorder.size()){
+            return NULL;
+        }
+        
+        TreeNode* root = new TreeNode(preorder[i++]);
+
+        int in = findIndex(inorder, root->val);
+        cout<<in<<" ";
+        root->left = rec(preorder, inorder, i, l, in-1);
+        root->right = rec(preorder, inorder, i, in+1, r);
+        
+        return root;
+    }
+
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        int i =0;
+        TreeNode* root = rec( preorder, inorder, i, 0, n-1);
+        return root;
+    }
+};
+
+
+
+
+
+/********* finding index of root and passing ******/
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
