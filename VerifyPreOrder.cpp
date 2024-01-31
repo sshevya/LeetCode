5c77de8bd93e83eda432f48284e176d94df7b265
sshevya <37146723+sshevya@users.noreply.github.com>
2024-01-31 07:46:24 +0530

    Create VerifyPreOrder.cpp

---
+++ VerifyPreOrder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+        
+    bool verifyPreorder(vector<int>& preorder) {
+        stack<int> s;
+
+        int lower_bound = INT_MIN;
+
+        for(int i=0;i<preorder.size();i++){
+            if(s.empty() || preorder[i]<preorder[i-1]){
+                if(preorder[i]<lower_bound){
+                    return false;
+                }
+                s.push(preorder[i]);
+            }
+            else{
+                while(!s.empty() && s.top()<preorder[i]){
+                    lower_bound = s.top();
+                    s.pop();
+                }
+
+                s.push(preorder[i]);
+            }
+        }
+        return true;
+    }
+};
